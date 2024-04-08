0a9dec281567001c1540a4c6f8566165eff8fdd5
andremartins <andremartins@frwk.com.br>
2024-04-08 18:48:33 -0300

    organizing repo

--- src/main/java/com/vicarius/quota/repository/mysql/MySqlUserRepository.java
+++ src/main/java/com/vicarius/quota/repository/mysql/MySqlUserRepository.java
@@ -1,11 +1,12 @@
-package com.vicarius.quota.repository;
+package com.vicarius.quota.repository.mysql;
 
 import com.vicarius.quota.model.User;
+import com.vicarius.quota.repository.GenericRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("mySqlUserRepository")
-public interface UserMySqlRepository extends CrudRepository<User, Long>, GenericRepository<User, Long> {
+public interface MySqlUserRepository extends CrudRepository<User, Long>, GenericRepository<User, Long> {
 
     default Class<User> getEntityClass() {
         return User.class;
